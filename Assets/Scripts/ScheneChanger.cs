a79e2a019fbe3ba2a0111182e06cf6a795414e27
Alcestra <torje.tonsager@gmail.com>
2021-04-13 22:49:21 +0200

    Initial start screen and 1 part of the roads + sound

---
+++ Assets/Scripts/ScheneChanger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScheneChanger : MonoBehaviour
+{
+
+    public Button button;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(ChangeScene);
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("ButtonPressed");
+
+    }
+}
